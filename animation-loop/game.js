ae6df30d819ab7d37514b5d502deb047d0e42431
Yogesh Kumar <yogeshkumar@197NODNB13718.(none)>
2014-11-07 12:32:09 +0530

    Worked on animation loop

--- animation-loop/game.js
+++ animation-loop/game.js
@@ -1,11 +1,16 @@
 var canvas = document.getElementById('game');
+
+canvas.width = 1024;
+canvas.height = 800;
+
 var context = canvas.getContext('2d');
 
-function Box() {
-    this.x = 10;
-    this.y = 10;
-    this.width = 100;
-    this.height = 100;
+function Box (options) {
+    this.x = (options.x) ? options.x : 10;
+    this.y = (options.y) ? options.y : 10;
+    this.width = (options.width) ? options.width : 100;
+    this.height = (options.height) ? options.height : 100;
+    this.color = (options.color) ? options.color : '#000';
 }
 
 var box = new Box();
@@ -13,13 +18,19 @@ var boxes = [];
 var totalBoxes = 100;
 
 for(var i=0; i<100; i++){
-    boxes[i] = new Box();
+    boxes[i] = new Box({
+        x: 10+i,
+        y: 10+i,
+        width: 2*i,
+        height: 3*i,
+        color: randomColor(0, 255, 0, 255, 0, 255, .5)
+    });
 }
 
 function update() {
   boxes.forEach(function(box, i){
-    box.x = Math.random() < 0.5 ? -1 : 1;
-    box.y = Math.random() < 0.5 ? -1 : 1;
+    box.x += randomNumber(-5, 5);
+    box.y += randomNumber(-5, 5);
   });  
 }
 
@@ -28,6 +39,7 @@ function draw() {
     //x, y, width, height
     
     boxes.forEach(function(box, i) {
+        context.fillStyle = box.color;
         context.fillRect(box.x, box.y, box.width, box.height);
     });
 
@@ -37,8 +49,18 @@ function loop() {
     update();
     draw();
     window.requestAnimationFrame(loop);
-    //This will call the loop function on every frame, i.e. 30 fs or 60 fps
+    //This will call the loop function on every frame, i.e. 30 fps or 60 fps
     //according to the browser speed.
 }
 
+function randomNumber(min, max){
+    return Math.round(Math.random()* (max - min + 1) + min);
+}
+
+function randomColor(rmin, rmax, gmin, gmax, bmin, bmax, alpha){
+    var r = randomNumber(rmin, rmax);
+    var g = randomNumber(gmin, gmax);
+    var b = randomNumber(bmin, gmax);
+    return 'rgba(' + r + ', ' + g + ', ' + b + ', ' + alpha + ')';
+}
 loop();
